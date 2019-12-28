87ed69c9e88a749fa74cb613e65b29381f815ac0
AspidDark <aupovTR@gmail.com>
2019-12-28 23:29:48 +0300

    Docker Setup+ base Integration tests (xUnit)

---
+++ TB.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using TB.Contracts.V1;
+using TB.Contracts.V1.Requests;
+using TB.Contracts.V1.Responses;
+using TB.Data;
+using Xunit;
+
+namespace TB.IntegrationTests
+{
+    //nuget
+    //Microsoft.NETCore.App
+    //Microsoft.AspNetCore.Mvc.Testing
+    //Microsoft.AspNetCore.App
+    //FluentAssertions
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestClient;
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(bulider=>
+                {
+                    //in memory Database creating
+                    bulider.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DataContext));
+                        services.AddDbContext<DataContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+            TestClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new 
+                AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        protected async Task<PostResponse> CreatePostAsync(CreatePostRequest request)
+        {
+            var response= await TestClient.PostAsJsonAsync(ApiRoutes.Posts.Create, request);
+            return await response.Content.ReadAsAsync<PostResponse>();
+
+        }
+
+
+
+
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Identity.Register, new UserRegistrationRequest
+            {
+                Email="test@integration.com",
+                Password="SomePass1234!"
+            });
+
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+            return registrationResponse.Token;
+        }
+    }
+}
---
+++ TB.IntegrationTests/PostControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TB.Contracts.V1;
+using TB.Domain;
+using Xunit;
+
+namespace TB.IntegrationTests
+{
+    public class PostControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutAnyPosts_ReturnsEmptyResponse()
+        {
+            //Arrage
+            await AuthenticateAsync();
+            //Act
+            var response = await TestClient.GetAsync(ApiRoutes.Posts.GetAll);
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsPost_WhenPostExistsInTheDatabase()
+        {
+            //Arrage
+            await AuthenticateAsync();
+            var createdPost = await CreatePostAsync(new Contracts.V1.Requests.CreatePostRequest {Name= "Test post" });
+            //Act
+            var response = await TestClient
+                .GetAsync(ApiRoutes.Posts.Get.Replace("{postId}", createdPost.Id.ToString()));
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedPost = await response.Content.ReadAsAsync<Post>();
+            returnedPost.Id.Should().Be(createdPost.Id);
+            returnedPost.Name.Should().Be("Test post");
+        }
+
+    }
+}
