c310471eebd79d82d474819b37b6406ffc29273a
Tim <TAyupov@bistrodengi.ru>
2020-01-15 14:17:06 +0300

    Comments Added

--- TB/Startup.cs
+++ TB/Startup.cs
@@ -27,7 +27,7 @@ namespace TB
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // Optional() регистрирует сервисы, которые используются приложением
         public void ConfigureServices(IServiceCollection services)
         {
             services.InstallServicesInAssemblies(Configuration);
